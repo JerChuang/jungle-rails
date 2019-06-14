bcd42c1f9c4a1fa4f1f14e3abf8ea661806ad066
Jerry Chuang <jerry@Jerrys-MacBook-Air.local>
2019-06-14 15:35:03 -0700

    Add logic for reviews controller

---
+++ app/controllers/reviews_controller.rb
@@ -0,0 +1,15 @@
+class ReviewsController < ApplicationController
+  def create
+
+    @product = Product.find(params['product_id'])
+    
+    @review = @product.reviews.create product_id: params['product_id'], rating: params['review']['rating'], description: params['review']['description'], user: current_user
+
+    if @review.valid?
+      redirect_to @product
+    else
+      redirect_to @product
+    end
+
+  end
+end
