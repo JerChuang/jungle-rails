1f3c48d2489c99491eee56f3486175de4971b498
Jerry Chuang <jerry@Jerrys-MacBook-Air.local>
2019-06-14 17:23:20 -0700

    Add validation for user

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,12 @@ class User < ActiveRecord::Base
   has_secure_password
 
   has_many :reviews
+
+
+  validates :first_name, presence: true
+  validates :last_name, presence: true
+  validates :email, presence: true, uniqueness: true
+  validates :password_digest, presence: true
+
 end
+
